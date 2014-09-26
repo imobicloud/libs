a90b806e6ddf7e0b6294b256d33b16cea743c8ba
Pham Thanh Quang <ptquang86@gmail.com>
2014-09-26 10:24:37 +0700

    update comment

--- managers/nav.js
+++ managers/nav.js
@@ -54,8 +54,12 @@ icon size:
 		+ 2x: 44
 		+ 1x: 22 
 	- android 
-		+ Full asset, 32x32 dp
-		+ Optical square, 24x24 dp
+		[Optical square] area in [Full asset]
+		24 × 24 		 area in 32 × 32 (mdpi)
+		36 × 36 		 area in 48 × 48 (hdpi)
+		48 × 48 		 area in 64 × 64 (xhdpi)
+		72 × 72 		 area in 96 × 96 (xxhdpi)
+		96 × 96 		 area in 128 × 128 (xxxhdpi)
 * */
 exports.load = navigation.load;
 exports.update = navigation.update;
