f9ba227f83655e8171a57c3bf65b692a2a7832ea
ptquang86 <ptquang86@gmail.com>
2015-09-25 16:10:49 +0700

    libs: tab groups: update demo

--- managers demos/win-tabgroup/app/controllers/index.js
+++ managers demos/win-tabgroup/app/controllers/index.js
@@ -1,16 +1,18 @@
 init();
 
 function init() {
-	var oPlugins = require('managers/plugins'),
-		plugins  = new oPlugins('window', { navigation: true });
-		
-	Alloy.Globals.toggleAI = plugins.toggleAI;
-	Alloy.Globals.updateNav = plugins.updateNav;
-	
 	// initialize window manager
 	
+	// var oPlugins = require('managers/plugins'),
+		// plugins  = new oPlugins('window', { ai: false, keyboard: false, toast: false });
+		
 	var oWindowManager = require('managers/window'),
-		winManager = new oWindowManager( plugins.windowChanged );
+		winManager = new oWindowManager();
+		
+	// winManager
+		// .on('window:show', plugins.windowShow)
+		// .on('window:hide', plugins.windowHide);
+	OS_ANDROID && winManager.on('window:exit', exitConfirm);
 	
 	Alloy.Globals.WinManager = winManager;
 	
@@ -25,4 +27,19 @@ function init() {
 			url: 'login'
 		});
 	}
+}
+
+function exitConfirm() {
+    // var dialog = Ti.UI.createAlertDialog({
+        // cancel : 0,
+        // buttonNames : ['NO', 'YES'],
+        // title : 'Quit?',
+        // message: 'Are you sure?',
+    // });
+	// dialog.addEventListener('click', function(e) {
+		// if (e.index !== e.source.cancel) {
+			Alloy.Globals.WinManager.exit();
+		// }
+	// });
+	// dialog.show();
 }
