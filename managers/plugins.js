3ba6976073939ce6fb9b6c7caf8af2672428e61c
ptquang86 <ptquang86@gmail.com>
2015-10-23 14:58:08 +0700

    update plugins

--- managers/plugins.js
+++ managers/plugins.js
@@ -36,7 +36,7 @@ function Plugins(config) {
 			params._ai.unload();
 	  		params._ai = null;
 		}
-		config.keyboard && (params._txt = null);
+		config.keyboard && !OS_ANDROID && (params._txt = null);
 	}
 	
 	/* =============================================================================
@@ -44,7 +44,7 @@ function Plugins(config) {
    ========================================================================== */
 	  
 	function loadAI(params, win) {
-		var ai = Alloy.createWidget('com.imobicloud.ai', { visible: win.hasAI != 'false' });
+		var ai = Alloy.createWidget('com.imobicloud.ai', { visible: win.hasAI + '' != 'false' });
 	  	params._ai = ai;
 	  	win.add( ai.getView() );
 	}
@@ -78,16 +78,19 @@ function Plugins(config) {
 	
 	function loadKeyboard(params, win) {
 	  	// attach hidden textfield for hiding keyboard
-		
-		var txt = Ti.UI.createTextField({ visible: false });
-		// OS_ANDROID && (txt.softKeyboardOnFocus = Ti.UI.Android.SOFT_KEYBOARD_HIDE_ON_FOCUS);
-		params._txt = txt;
-		win.add(txt);
+	  	
+		if (!OS_ANDROID) {
+			var txt = Ti.UI.createTextField({ visible: false });
+			params._txt = txt;
+			win.add(txt);
+		}
 		
 		// hide keyboard on tap
 		
-		if (win.hasWebview != 'true') {
-			win.addEventListener('singletap', function(e) {
+		if (win.hasWebview + '' != 'true') {
+			// click: window is click-able, list-view is not click-able
+			// singletap: window is not click-able, list-view is click-able
+			win.addEventListener('click', function(e) {
 				if ( ['Ti.UI.TextField', 'Ti.UI.TextArea', 'Ti.UI.SearchBar', 'Ti.UI.Android.SearchView'].indexOf( e.source.apiName ) == -1 ) {
 					hideKeyboard();
 				}
@@ -96,9 +99,9 @@ function Plugins(config) {
 	}
 	  
 	function hideKeyboard() {
-		//TODO: try this Ti.UI.Android.hideSoftKeyboard();
-		
-		if (OS_ANDROID || Ti.App.keyboardVisible) { // Ti.App.keyboardVisible for iOS only
+		if (OS_ANDROID) {
+			Ti.UI.Android.hideSoftKeyboard();
+		} else if (Ti.App.keyboardVisible) {
 			var current = Alloy.Globals.WinManager.getCache(-1);
 		  	if (current == null) {
 				current = Alloy.Globals.Tabgroup.getCache(-1, -1);
