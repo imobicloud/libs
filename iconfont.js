079d7871e7b3d2cde41ff6fb298b72b681c276b0
ptquang86 <ptquang86@gmail.com>
2016-11-30 13:42:23 +0700

    iconfont: support Ionicons

--- iconfont.js
+++ iconfont.js
@@ -1,7 +1,17 @@
 var defaultFont;
 var maps = {};
 
-exports.init = function(fontName) {
+/*
+// Ionicons
+require('iconfont').init('ionicons', function(fontObj, maps, fontName) {
+	var icons = fontObj.icons;
+	for (var i = 0, ii = icons.length; i < ii; i++) {
+		var properties = icons[i];
+		maps[fontName][properties.name] = String.fromCharCode(parseInt(properties.code, 16));
+	}
+});
+*/
+exports.init = function(fontName, fontParser) {
 	maps[fontName] = {};
 	
 	if (defaultFont == null) {
@@ -17,52 +27,58 @@ exports.init = function(fontName) {
 	try {
 		var fontObj = JSON.parse(fontFile.read().text);
 		
-		if (Array.isArray(fontObj)) {
-			// https://materialdesignicons.com/
-			// Map: copied from preview.html in download package
-			// iconfont-ex-5.json
-			if (fontObj[0].hex) {
-				for (var i = 0, ii = fontObj.length; i < ii; i++) {
-					var properties = fontObj[i];
-					maps[fontName][properties.name] = String.fromCharCode(parseInt(properties.hex, 16));
-				}
-			} 
-			// https://github.com/MattMcFarland/com.mattmcfarland.fontawesome/blob/master/lib/icons.js
-			// iconfont-ex-1.json
-			else {
-				for (var i = 0, ii = fontObj.length; i < ii; i++) {
-					var properties = fontObj[i];
-					maps[fontName][properties[0]] = String.fromCharCode(properties[1]);
-				}
-			}
+		if (fontParser) {
+			fontParser(fontObj, maps, fontName);
 		} else {
-			var fontMap = fontObj.icons;
-			if (fontMap) {
-				// IcoMoon style
-				// iconfont-ex-2.json
-				if (Array.isArray(fontMap)) {
-					for (var i = 0, ii = fontMap.length; i < ii; i++) {
-						var properties = fontMap[i].properties;
-						maps[fontName][properties.name] = String.fromCharCode(properties.code);
+			
+			if (Array.isArray(fontObj)) {
+				// https://materialdesignicons.com/
+				// Map: copied from preview.html in download package
+				// iconfont-ex-5.json
+				if (fontObj[0].hex) {
+					for (var i = 0, ii = fontObj.length; i < ii; i++) {
+						var properties = fontObj[i];
+						maps[fontName][properties.name] = String.fromCharCode(parseInt(properties.hex, 16));
 					}
 				} 
-				// Google Material Icon
-				// iconfont-ex-4.json
-				// Font: https://github.com/google/material-design-icons/tree/master/iconfont
-				// Map: https://github.com/google/material-design-icons/blob/master/iconfont/MaterialIcons-Regular.ijmap
+				// https://github.com/MattMcFarland/com.mattmcfarland.fontawesome/blob/master/lib/icons.js
+				// iconfont-ex-1.json
 				else {
-					for (var key in fontMap) {
-						maps[fontName][fontMap[key].name] = String.fromCharCode('0x' + key);
+					for (var i = 0, ii = fontObj.length; i < ii; i++) {
+						var properties = fontObj[i];
+						maps[fontName][properties[0]] = String.fromCharCode(properties[1]);
 					}
 				}
-			} 
-			// https://github.com/k0sukey/TiIconicFont/blob/master/Resources/lib/FontAwesome.js
-			// iconfont-ex-3.json
-			else {
-				for (var key in fontObj) {
-					maps[fontName][key] = String.fromCharCode(fontObj[key]);
+			} else {
+				var fontMap = fontObj.icons;
+				if (fontMap) {
+					// IcoMoon style
+					// iconfont-ex-2.json
+					if (Array.isArray(fontMap)) {
+						for (var i = 0, ii = fontMap.length; i < ii; i++) {
+							var properties = fontMap[i].properties;
+							maps[fontName][properties.name] = String.fromCharCode(properties.code);
+						}
+					} 
+					// Google Material Icon
+					// iconfont-ex-4.json
+					// Font: https://github.com/google/material-design-icons/tree/master/iconfont
+					// Map: https://github.com/google/material-design-icons/blob/master/iconfont/MaterialIcons-Regular.ijmap
+					else {
+						for (var key in fontMap) {
+							maps[fontName][fontMap[key].name] = String.fromCharCode('0x' + key);
+						}
+					}
+				} 
+				// https://github.com/k0sukey/TiIconicFont/blob/master/Resources/lib/FontAwesome.js
+				// iconfont-ex-3.json
+				else {
+					for (var key in fontObj) {
+						maps[fontName][key] = String.fromCharCode(fontObj[key]);
+					}
 				}
 			}
+			
 		}
 	} catch (fontParseError) {
 		console.log('*** iconfont: fontParseError: ' + fontParseError);
